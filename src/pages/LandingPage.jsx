f77216cd75055ea7b55670b1296272eadfef81a4
bridgetorr17 <bridgetorr1902@gmail.com>
2025-07-21 19:49:43 -0400

    setting up tailwind css

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const LandingPage = () => {
     return (
         <>  
-            <h1>Welcome to your Trip Planner and Reminiscer</h1>
+            <h1 className="underline">Welcome to your Trip Planner and Reminiscer</h1>
             <Link to="/login">Login</Link>
             <Link to="/signup">Signup</Link>
             <Link to="/dashboard/">ViewTrips</Link>
