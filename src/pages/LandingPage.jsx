36e7020a5404f26ac0de9c4a8da2ee7f4cdd739a
bridgetorr17 <bridgetorr1902@gmail.com>
2025-07-23 18:47:53 -0400

    landing page styles

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,13 +1,21 @@
 import { Link } from "react-router-dom";
+import bg from '../photos/landingPageBackground.jpg'
 
 const LandingPage = () => {
     return (
-        <>  
-            <h1 className="underline">Welcome to your Trip Planner and Reminiscer</h1>
-            <Link to="/login">Login</Link>
-            <Link to="/signup">Signup</Link>
-            <Link to="/dashboard/">ViewTrips</Link>
-        </>
+        <div className="flex flex-col justify-center items-center"
+            style={{
+                backgroundImage: `url(${bg})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                height: '100vh'
+                }}>
+            <h1 className="text-7xl p-2 text-gray-50">TRIPLY</h1>
+            <span className="text-4xl p-2 text-gray-50">Hold on to the memories, they will hold on to you.</span>
+            <Link to="/login" className="bg-teal-400 text-gray-50 font-medium rounded-full px-6 py-3 shadow-md transition transform hover:scale-105 hover:bg-teal-600 focus:outline-none focus:ring-3 focus:ring-teal-300 p-2 m-2">Login</Link>
+            <Link to="/signup" className="bg-teal-400 text-gray-50 font-medium rounded-full px-6 py-3 shadow-md transition transform hover:scale-105 hover:bg-teal-600 focus:outline-none focus:ring-3 focus:ring-teal-300 p-2 m-2">Signup</Link>
+            <Link to="/dashboard/" className="bg-teal-400 text-gray-50 font-medium rounded-full px-6 py-3 shadow-md transition transform hover:scale-105 hover:bg-teal-600 focus:outline-none focus:ring-3 focus:ring-teal-300 p-2 m-2">See your trips</Link>
+        </div> 
     )
 }
 
