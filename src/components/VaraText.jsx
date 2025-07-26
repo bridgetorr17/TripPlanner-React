6b8e0e6cee42c8580f9437b48f5642328e0c156c
bridgetorr17 <bridgetorr1902@gmail.com>
2025-07-26 19:15:13 -0400

    front page Triply animation

---
+++ src/components/VaraText.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useRef } from 'react';
+import Vara from 'vara';
+
+function VaraText({ text, fontURL, options }) {
+
+    const containerRef = useRef(null);
+
+    useEffect(() => {
+        if (!containerRef.current) return;
+        if (!containerRef.current.id) containerRef.current.id = 'vara-container';
+        const vara = new Vara(
+            `#${containerRef.current.id}`,
+            fontURL,
+            [{ text, ...options}],
+            { color: options.color, fontSize: options.fontSize, strokeWidth: options.strokeWidth, textAlign: 'center' }
+        );
+        return () => vara.remove();
+    }, [text, fontURL, options])
+
+    return (
+        <div ref={containerRef}/>
+    )
+}
+
+export default VaraText;
