5c1746010d7cfaf0b0403129f6fc82c7c900eaf8
bridgetorr17 <bridgetorr1902@gmail.com>
2025-08-29 18:14:01 -0400

    adding autocomplete to location search

--- src/components/Locations.jsx
+++ src/components/Locations.jsx
@@ -1,12 +1,13 @@
 import Map from "./Map";
 import List from "./List";
 import DynamicListInput from "./DynamicListInput";
+import PlaceAutocomplete from "./PlaceAutocomplete";
 
 const Locations = ({editMode, locations, setLocations}) => {
 
     return (
         <div className="space-y-4 p-4 bg-sky-50 rounded-md">
-            <div className="flex flex-row justify-center items-stretch space-x-4">
+            <div className="flex flex-row justify-around items-stretch space-x-4">
                 {editMode ? 
                     <div className="flex-1 p-4">
                         <div className="flex-1 h-full">
@@ -24,10 +25,9 @@ const Locations = ({editMode, locations, setLocations}) => {
                                 <List arr={locations} links={false} />
                             </div>
                         </div>}
-                <div className="flex-1 p-2 rounded shadow-sm flex flex-col h-[200px]">
-                    <div className="flex-1 h-full flex justify-center">
+                <div className="flex-1 flex flex-col p-2 rounded shadow-sm relative h-[200px]">
+                        <PlaceAutocomplete />
                         <Map />
-                    </div>
                 </div>
             </div>
         </div>
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,6 +1,5 @@
 import { useCallback, useRef, useState } from "react";
 import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
-import SearchBox from "./SearchBox";
 
 const center = {
     lat: 40.7128,
@@ -22,32 +21,16 @@ const Map = () => {
         mapRef.current = map;
     }, [])
 
-    const [markers, setMarkers] = useState([]);
-
-    const handleAddMarker = (location) => {
-        setMarkers((current) => [...current, location]);
-        mapRef.current.panTo(location);
-        mapRef.current.setZoom(15);
-    }
-
     if (loadError) return <div>Error loading maps</div>;
     if (!isLoaded) return <div>Loading...</div>;
 
     return (
-        <div className="flex flex-col h-full w-full space-y-2">
-            <SearchBox onSelect={handleAddMarker} />
-            <div className="flex-1 rounded-md overflow-hidden shadow">
-                <GoogleMap
-                    mapContainerStyle={{ width: '100%', height: '100%' }}
-                    center={center}
-                    zoom={10}
-                    onLoad={onMapLoad}>
-                    {markers.map((loc, ind) => (
-                        <Marker key={ind} position={loc} />
-                    ))}
-                </GoogleMap>
-            </div>
-        </div>
+        <GoogleMap
+            mapContainerStyle={{ width: '100%', height: '100%' }}
+            center={center}
+            zoom={10}
+            onLoad={onMapLoad}>
+        </GoogleMap>
     )
 }
 
