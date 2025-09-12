c36729b249331d7c84b43a9a53fe210f20c85320
bridgetorr17 <bridgetorr1902@gmail.com>
2025-09-12 16:57:52 -0400

    adding reset password page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import {  Route,
           Outlet } from 'react-router-dom'
 import LandingPage from "./pages/LandingPage";
 import LoginPage from './pages/LoginPage';
+import ResetPasswordPage from './pages/ResetPasswordPage';
 import ErrorPage from './pages/ErrorPage';
 import SignupPage from './pages/SignupPage';
 import DashboardPage from './pages/DashboardPage';
@@ -68,6 +69,7 @@ const App = () => {
                 <Route index element={<LandingPage />} errorElement={<ErrorPage/>}/>
                 <Route path='/login' element={<LoginPage loginAttempt={loginAttempt}/>} loader={loginLoader} errorElement={<ErrorPage/>}/>
                 <Route path='/signup' element={<SignupPage signupAttempt={signupAttempt} />} errorElement={<ErrorPage/>}/>
+                <Route path='/resetPassword' element={<ResetPasswordPage />} errorElement={<ErrorPage/>}/>
                 <Route path='/preview' element={<PreviewPage/>} errorElement={<ErrorPage/>}/>
                 <Route path='/dashboard' element={<DashboardPage/>} loader={dashboardLoader} errorElement={<ErrorPage/>}/>
                 <Route path='/dashboard/:userName' element={<UserPage/>} loader={userLoader} errorElement={<ErrorPage/>}/>
---
+++ src/pages/ResetPasswordPage.jsx
@@ -0,0 +1,75 @@
+import { useState } from "react";
+import Spinner from "../components/Utlities/Spinner";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+
+const ResetPasswordPage = () => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [showPassword, setShowPassword] = useState(false)
+    const [confirmPassword, setConfrimPassword] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    const submitForm = async (e) => {
+        e.preventDefault();
+
+        console.log('requesting a reset password')
+        
+    }
+
+    return (
+        <div className="min-h-screen bg-blue-400 flex items-center justify-center px-4 sm:px-6 md:px-8">
+            <div className="bg-white max-w-md w-full p-8 rounded-lg shadow-xl border border-cyan-200">
+                <h2 className="text-2xl font-semibold text-blue-700 text-center mb-6">Reset Password</h2>
+                <form onSubmit={submitForm} className="flex flex-col gap-4">
+                    <input 
+                        type="email"
+                        name="email" 
+                        placeholder="Email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        className="px-4 py-2 border-2 border-sky-300 rounded-md focus:outline-none focus:border-blue-400 transition"
+                    />
+                    <div className="flex flex-row justify-between items-center px-4 py-2 border-2 border-sky-300 rounded-md focus:outline-none focus:border-blue-400 transition">
+                        <input 
+                            type={showPassword ? "text" : "password"}
+                            name="password" 
+                            placeholder="New password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)} 
+                            className="focus:outline-none focus:ring-0"
+                            disabled={loading}
+                        />
+                        <button
+                            type="button"
+                            onClick={(e) => setShowPassword(prev => !prev)}
+                            className=" text-blue-500 hover:text-blue-700"
+                            tabIndex={-1}
+                        >
+                            {showPassword ? <FaEyeSlash /> : <FaEye />}
+                        </button>
+                    </div>
+                    <input 
+                        type="password"
+                        name="confirmPassword" 
+                        placeholder="Confirm new password"
+                        value={confirmPassword}
+                        onChange={(e) => setConfrimPassword(e.target.value)} 
+                        className="px-4 py-2 border-2 border-sky-300 rounded-md focus:outline-none focus:border-blue-400 transition"
+                    />
+                    <button type="submit"
+                            className={`mt-4 py-2 bg-blue-600 hover:bg-blue-700 text-white rounded-md font-medium transition
+                                        ${loading
+                                            ? "bg-blue-400 cursor-not-allowed"
+                                            : "bg-blue-600 hover:bg-blue-700 text-white"
+                                        }`}>
+                        Reset Password
+                    </button>
+                </form>
+            </div>
+        </div>
+
+    )
+}
+
+export default ResetPasswordPage;
