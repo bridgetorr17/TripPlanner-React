a440add69d3ee2b0f29e12e0d7a47157c5428e9b
Margaret Wismer <margaretwismer@Margarets-Air.fios-router.home>
2025-09-22 20:40:20 -0400

    add trip page components

---
+++ src/components/Trip/FeaturePanel.jsx
@@ -0,0 +1,29 @@
+import FeatureHeader from "./FeatureHeader"
+
+const FeaturePanel = ({
+    userStatus,
+    toggleEdit,
+    editFeature,
+    setEditFeature,
+    headerTitle,
+    children
+}) => {
+    return (
+        <section className="bg-white border border-sky-200 rounded-lg shadow-md p-6 space-y-4">
+            {(userStatus !== 'viewer') && <FeatureHeader 
+                headerTitle={headerTitle}                      
+                modifyText={editFeature ? "Cancel" : "Add"}
+                onToggleEdit={() => 
+                    toggleEdit(
+                        editFeature, 
+                        () => {setEditFeature(false)},
+                        setEditFeature
+                        )
+                    }
+                />}
+                {children}
+        </section>
+    )
+}
+
+export { FeaturePanel as default }
