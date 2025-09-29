59d5be283b1bd758332c37dc593f0da080f9beb9
bridgetorr17 <bridgetorr1902@gmail.com>
2025-09-29 18:28:52 -0400

    autocomplete place adjusts center

--- src/components/Location/Locations.jsx
+++ src/components/Location/Locations.jsx
@@ -23,27 +23,29 @@ const Locations = ({editMode, locations, setLocations, tripId}) => {
         setNewPlace(selectedPlace);
         const placeId = selectedPlace.placePrediction.placeId;
         try {
-        const response = await fetch( `https://places.googleapis.com/v1/places/${placeId}`,
-            {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json",
-                    "X-Goog-Api-Key": import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
-                    "X-Goog-FieldMask": "location"
-                },
-            }
-        );
+            const response = await fetch( `https://places.googleapis.com/v1/places/${placeId}`,
+                {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json",
+                        "X-Goog-Api-Key": import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
+                        "X-Goog-FieldMask": "location"
+                    },
+                }
+            );
 
-        const result = await response.json();
+            const result = await response.json();
 
-        setNewCoords({
-            lat: result.location?.latitude,
-            lng: result.location?.longitude
-        })
-    } catch {
-        navigate('/errorpage')
-    }
-        setCenterCoords([result.location?.latitude, result.location?.longitude]);
+            setNewCoords({
+                lat: result.location?.latitude,
+                lng: result.location?.longitude
+            });
+
+            setCenterCoords([result.location?.latitude, result.location?.longitude]);
+        } 
+        catch {
+            navigate('/errorpage')
+        }
     }
 
     const addLocation = async () => {
