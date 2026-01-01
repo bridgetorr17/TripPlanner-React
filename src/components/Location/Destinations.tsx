d20a0663d1a14e9c857256eb6ca16fb05c8c4bc9
bridgetorr17 <bridgetorr1902@gmail.com>
2026-01-01 19:52:57 +0100

    different buttons for destination vs location

--- src/components/Location/Destinations.tsx
+++ src/components/Location/Destinations.tsx
@@ -50,81 +50,82 @@ const Destinations = ({editMode, destinations, setDestinations, tripId}: Destina
 
     return (
         <div>
-        <div className="flex flex-col sm:flex-row justify-around items-stretch space-y-4 sm:space-y-0 sm:space-x-6">
-            <div className="w-full sm:w-1/3 bg-white border border-gray-200 rounded shadow-sm p-4 overflow-y-auto">
-                {destinations.map((dest, ind) => (
-                    <div key={ind}>
-                        <div
-                            onClick={() => {
-                            setSelectedDest(ind === selectedDest ? null : ind);
-                            setMapLocations(dest.locations);
-                            setCoords([dest.coordinates.latitude, dest.coordinates.longitude]);
-                            }}
-                            className={`cursor-pointer px-3 py-2 rounded transition-all duration-200${selectedDest === ind ? "bg-blue-100 font-bold text-lg text-blue-700" : "hover:bg-gray-100 text-gray-800"}`}>
-                                {dest.name.mainText}
-                        </div>
-                        {selectedDest === ind && (
-                            <div className="mt-2 ml-5 space-y-1 transition-all duration-300">
-                                {dest.locations.map((loc, idx) => (
-                                    <div
-                                        key={idx}
-                                        onClick={() =>
-                                            setCoords([loc.coordinates.latitude, loc.coordinates.longitude])
-                                        }
-                                        className="cursor-pointer text-sm text-gray-700 px-3 py-1 rounded hover:bg-gray-100"
-                                        >
-                                            {loc.name.mainText}
-                                    </div>
-                                ))}
+            <div className="flex flex-col sm:flex-row justify-around items-stretch space-y-4 sm:space-y-0 sm:space-x-6">
+                <div className="w-full sm:w-1/3 bg-white border border-gray-200 rounded shadow-sm p-4 overflow-y-auto">
+                    {destinations.map((dest, ind) => (
+                        <div key={ind}>
+                            <div
+                                onClick={() => {
+                                setSelectedDest(ind === selectedDest ? null : ind);
+                                setMapLocations(dest.locations);
+                                setCoords([dest.coordinates.latitude, dest.coordinates.longitude]);
+                                }}
+                                className={`cursor-pointer px-3 py-2 rounded transition-all duration-200${selectedDest === ind ? "bg-blue-100 font-bold text-lg text-blue-700" : "hover:bg-gray-100 text-gray-800"}`}>
+                                    {dest.name.mainText}
                             </div>
-                        )}
-                    </div>
-                ))}
-                
+                            {selectedDest === ind && (
+                                <div className="mt-2 ml-5 space-y-1 transition-all duration-300">
+                                    {dest.locations.map((loc, idx) => (
+                                        <div
+                                            key={idx}
+                                            onClick={() =>
+                                                setCoords([loc.coordinates.latitude, loc.coordinates.longitude])
+                                            }
+                                            className="cursor-pointer text-sm text-gray-700 px-3 py-1 rounded hover:bg-gray-100"
+                                            >
+                                                {loc.name.mainText}
+                                        </div>
+                                    ))}
+                                </div>
+                            )}
+                        </div>
+                    ))}
+                    
+                </div>
+                <div className="sm:flex-1 flex flex-col p-2 rounded shadow-sm relative h-64 sm:h-[250px] md:h-[350px]">
+                    <PlaceAutocomplete 
+                        editMode={editMode}
+                        handleSelect={selectNewPlace}
+                        clearPlace={true}/>
+                    <Map locations={mapLocations} coords={coords} />
+                </div>
             </div>
-            <div className="sm:flex-1 flex flex-col p-2 rounded shadow-sm relative h-64 sm:h-[250px] md:h-[350px]">
-                <PlaceAutocomplete 
-                    editMode={editMode}
-                    handleSelect={selectNewPlace}
-                    clearPlace={true}/>
-                <Map locations={mapLocations} coords={coords} />
+            <div>
+                {newPlace && editMode && (
+                    <div className="mt-4 p-4 bg-green-50 border border-green-300 rounded-lg">
+                        <div className="mb-3 text-lg font-semibold text-green-800">
+                            <span 
+                                className="inline-block transform hover:scale-105 transition-transform duration-200 ease-in-out cursor-pointer"
+                                onClick={() => setCoords([...newPlaceCoords])}>
+                                {newPlace.placePrediction?.structuredFormat?.mainText?.text}
+                            </span>
+                        </div>
+                        <div className="mb-3 text-sm font-medium text-green-800">
+                            {newPlace.placePrediction?.structuredFormat?.secondaryText?.text}
+                        </div>
+                        {(selectedDest !== null) && 
+                            <div>
+                                <button
+                                    className="w-full flex justify-center items-center gap-2 bg-teal-600 hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-teal-500 text-white text-base font-semibold py-1 rounded-lg transition"
+                                    //onClick={addLocation}
+                                    >
+                                    Add this location to {destinations[selectedDest].name.mainText}
+                                </button>
+                                <div className="flex items-center justify-center text-gray-500 font-medium">
+                                    <span className="px-2 bg-green-50 text-gray-600">or</span>
+                                </div>
+                            </div>
+                        }
+                        <button
+                            className="w-full flex justify-center items-center gap-2 bg-teal-600 hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-teal-500 text-white font-semibold py-1 rounded-lg transition"
+                            //onClick={addLocation}
+                            >
+                            Add as new destination
+                        </button>
+                    </div>
+                )}
             </div>
         </div>
-        <div>
-            {newPlace && editMode && (
-                        <div className="mt-4 p-4 bg-green-50 border border-green-300 rounded-lg">
-                            <div className="mb-3 text-lg font-semibold text-green-800">
-                                <span 
-                                    className="inline-block transform hover:scale-105 transition-transform duration-200 ease-in-out cursor-pointer"
-                                    onClick={() => setCoords([...newPlaceCoords])}>
-                                    {newPlace.placePrediction?.structuredFormat?.mainText?.text}
-                                </span>
-                            </div>
-                            <div className="mb-3 text-sm font-medium text-green-800">
-                                {newPlace.placePrediction?.structuredFormat?.secondaryText?.text}
-                            </div>
-                            {(selectedDest !== null) && 
-                                <div>
-                                    <button
-                                        className="w-full flex justify-center items-center gap-2 bg-teal-600 hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-teal-500 text-white text-base font-semibold py-1 rounded-lg transition"
-                                        //onClick={addLocation}
-                                        >
-                                        Add this location to {destinations[selectedDest].name.mainText}
-                                    </button>
-                                    <div className="flex items-center justify-center text-gray-500 font-medium">
-                                        <span className="px-2 bg-green-50 text-gray-600">or</span>
-                                    </div>
-                                </div>
-                            }
-                            <button
-                                className="w-full flex justify-center items-center gap-2 bg-teal-600 hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-teal-500 text-white font-semibold py-1 rounded-lg transition"
-                                //onClick={addLocation}
-                                >
-                                Add as new destination
-                            </button>
-                        </div>
-                    )}
-        </div></div>
     )
 }
 
